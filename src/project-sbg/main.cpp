c8896cecd56fef49d5ec3bf28c802d8d1a8ff167
Armen Berujanyan <armenberug@gmail.com>
2016-04-14 21:20:09 +0400

    Add project

---
+++ src/project-sbg/main.cpp
@@ -0,0 +1,6 @@
+/* * * * * * * * * * * * * * * * * * * * * * * *
+ * Project-SBG                                 *
+ * Version 0.0.16.4                            *
+ * Copyright (C) 2016 by NucleArc and other    *
+ * contributors                                *
+ * * * * * * * * * * * * * * * * * * * * * * * */
